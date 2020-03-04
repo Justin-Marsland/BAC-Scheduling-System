87090137579b74f176426077976b05a556194cb3
Justin-Marsland <49326379+Justin-Marsland@users.noreply.github.com>
2020-03-04 17:34:49 -0600

    Created Menu Class

---
+++ BACSchedulingSystem/BACSchedulingSystem/Models/Menu.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BACSchedulingSystem.Models
+{
+    public class Menu
+    {
+    }
+}

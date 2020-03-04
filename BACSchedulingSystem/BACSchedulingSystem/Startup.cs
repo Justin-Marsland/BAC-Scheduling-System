8f772745a4fb552f9e02ff723dacb0e38818e2e7
Justin-Marsland <49326379+Justin-Marsland@users.noreply.github.com>
2020-03-04 16:57:41 -0600

    Database take 2

--- BACSchedulingSystem/BACSchedulingSystem/Startup.cs
+++ BACSchedulingSystem/BACSchedulingSystem/Startup.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using BACSchedulingSystem.Data;
 
 namespace BACSchedulingSystem
 {
@@ -24,6 +21,12 @@ namespace BACSchedulingSystem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<BACContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("BACContext")));
+
+            services.AddDbContext<BACSchedulingSystemContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("BACSchedulingSystemContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

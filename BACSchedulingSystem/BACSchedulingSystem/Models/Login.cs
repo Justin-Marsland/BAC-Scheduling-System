4dd6ae64c1e32db582e3e40b9e50587d06546a53
purpledoop <60326139+purpledoop@users.noreply.github.com>
2020-03-27 10:51:01 -0500

    Created a skeletal Login page
    Access by adding /Login to end of url

---
+++ BACSchedulingSystem/BACSchedulingSystem/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BACSchedulingSystem.Models
+{
+    public class Login
+    {
+        public string username { get; set; }
+        public string email { get; set; }
+    }
+}
